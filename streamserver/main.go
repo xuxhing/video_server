e8b330a100b03fa5b8cecea90b493c4223d70dbb
sd1700092 <foreverskyone@hotmail.com>
2018-06-23 23:13:12 +0800

    finish 4-2

--- streamserver/main.go
+++ streamserver/main.go
@@ -7,8 +7,8 @@ import (
 
 func RegisterHandlers() *httprouter.Router {
 	router:=httprouter.New()
-	router.Get("/videos/:vid-id", streamHandler)
-	router.POSt("/upload/:vid-id", uploadHandler)
+	router.GET("/videos/:vid-id", streamHandler)
+	router.POST("/upload/:vid-id", uploadHandler)
 	return router
 }
 
